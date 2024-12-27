5ade805f31b369cf1c15f4aa10cda640b605c992
ROSTAND LELE <rostoinfocus@gmail.com>
2024-12-27 20:13:57 +0100

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="min-h-screen bg-slate-300 flex justify-center items-center px-32">
+  <div class="max-w-[1200px] w-full py-24 mx-auto flex flex-col items-center shadow-lg rounded-lg bg-white">
+
+  </div>
+
+  </div>
+</template>
+
+<script setup lang="ts">
+
+</script>
+
+<style scoped>
+
+</style>
